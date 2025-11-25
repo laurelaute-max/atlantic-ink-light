16cb8240ee9533da1468a5c2486eb803878c7ce8
laurelaute-max <laure.laute@gmail.com>
2025-11-25 20:41:56 +0000

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Ocean Project',
+  description: 'Exploration interactive de l’océan Atlantique',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
